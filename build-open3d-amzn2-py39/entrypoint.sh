c1d7a4bd183f307649baa71588540e7722ce6c82
Masahiro Fukuda <masahif+github@gmail.com>
2021-09-22 21:26:13 +0900

    added: open3d build environment with python3.9

---
+++ build-open3d-amzn2-py39/entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/sh -l
+set -eu
+
+echo "start entrypoint.sh"
+mkdir build
+cd build
+
+echo "run cmake"
+cmake \
+    -DBUILD_GUI=OFF \
+    -DUSE_SYSTEM_JPEG=ON \
+    -DBUILD_EXAMPLES=OFF \
+    -DDEVELOPER_BUILD=OFF \
+    -DPYTHON_EXECUTABLE=/usr/local/bin/python3.9 \
+    $GITHUB_WORKSPACE/Open3D
+
+echo "run make"
+make
+make pip-package
